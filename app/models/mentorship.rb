74350436f3f66a1ea5a5ef396b0ccab5c7c575c3
Mark McSpadden <markmcspadden@gmail.com>
2009-06-07 15:11:29 -0500

    Add states (accepted/rejected/completed) to Mentorships

--- app/models/mentorship.rb
+++ app/models/mentorship.rb
@@ -5,4 +5,40 @@ class Mentorship < ActiveRecord::Base
   belongs_to :sender, :class_name => "User"
   belongs_to :receiver, :class_name => "User"
 
+  # TODO: Metaprogram this
+  # states = [ ["accept", "accepted"], 
+  #             ["reject", "rejected"], 
+  #             ["complete", "completed"] ]
+  # I know acts_as_state exists, I just wanted to role my own to see how close it would match
+  def accepted?
+    !self.accepted_at.nil?
+  end
+  def accept(new_value = nil)
+    if new_value.is_a?(DateTime) || new_value.is_a?(Date) || new_value.is_a?(Time)
+      self.accepted_at = new_value
+    else
+      self.accepted_at = Time.now
+    end
+  end
+  def rejected?
+    !self.rejected_at.nil?
+  end
+  def reject(new_value = nil)
+    if new_value.is_a?(DateTime) || new_value.is_a?(Date) || new_value.is_a?(Time)
+      self.rejected_at = new_value
+    else
+      self.rejected_at = Time.now
+    end
+  end
+  def completed?
+    !self.completed_at.nil?
+  end
+  def complete(new_value = nil)
+    if new_value.is_a?(DateTime) || new_value.is_a?(Date) || new_value.is_a?(Time)
+      self.completed_at = new_value
+    else
+      self.completed_at = Time.now
+    end
+  end  
+
 end
--- spec/models/mentorship_spec.rb
+++ spec/models/mentorship_spec.rb
@@ -19,6 +19,84 @@ describe Mentorship do
     @mentorship.should be_valid
   end
   
+  describe "states" do
+    before(:each) do
+      @now = Time.now
+      Time.stub!(:now).and_return(@now)      
+    end
+    describe "accepted" do
+      it "should check to see if it has been accepted" do
+        @mentorship.accepted_at = nil
+        @mentorship.should_not be_accepted
+
+        @mentorship.accepted_at = Time.now
+        @mentorship.should be_accepted
+      end
+      it "should accept the mentorship and set the accepted_at time to now" do
+        Time.should_receive(:now).and_return(@now)
+      
+        @mentorship.accept
+        @mentorship.accepted_at.should == @now
+      end
+      it "should accept the mentorship and set the accepted_at time to a time passed in" do
+        @then = "2008-10-13".to_time
+      
+        Time.should_not_receive(:now).and_return
+      
+        @mentorship.accept(@then)
+        @mentorship.accepted_at.should == @then
+      end
+    end # accepted
+    describe "rejected" do
+      it "should check to see if it has been rejected" do
+        @mentorship.rejected_at = nil
+        @mentorship.should_not be_rejected
+
+        @mentorship.rejected_at = Time.now
+        @mentorship.should be_rejected
+      end
+      it "should accept the mentorship and set the rejected_at time to now" do
+        Time.should_receive(:now).and_return(@now)
+      
+        @mentorship.reject
+        @mentorship.rejected_at.should == @now
+      end
+      it "should accept the mentorship and set the rejected_at time to a time passed in" do
+        @then = "2008-10-13".to_time
+      
+        Time.should_not_receive(:now).and_return
+      
+        @mentorship.reject(@then)
+        @mentorship.rejected_at.should == @then
+      end
+    end # rejected
+    describe "completed" do
+      it "should check to see if it has been completed" do
+        @mentorship.completed_at = nil
+        @mentorship.should_not be_completed
+
+        @mentorship.completed_at = Time.now
+        @mentorship.should be_completed
+      end
+      it "should accept the mentorship and set the completed_at time to now" do
+        Time.should_receive(:now).and_return(@now)
+      
+        @mentorship.complete
+        @mentorship.completed_at.should == @now
+      end
+      it "should accept the mentorship and set the completed_at time to a time passed in" do
+        @then = "2008-10-13".to_time
+      
+        Time.should_not_receive(:now).and_return
+      
+        @mentorship.complete(@then)
+        @mentorship.completed_at.should == @then
+      end
+    end # completed    
+  end # state 
+  
+
+  
   describe "associations" do
     it "should have a mentor" do
       @mentorship.should respond_to("mentor")
