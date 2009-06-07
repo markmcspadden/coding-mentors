b1edf9dc9baee9ee0f6011fbf99af78a65e4e25f
Mark McSpadden <markmcspadden@gmail.com>
2009-06-07 15:38:32 -0500

    Add dummy current_user methods in ApplicationController and ApplicationHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+  
+  def current_user
+    @current_user ||= User.first
+  end
 end
