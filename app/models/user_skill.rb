ed62842ea86e0d42c185b2841d9f27e277536e96
Mark McSpadden <markmcspadden@gmail.com>
2009-06-08 06:56:07 -0500

    Add UserSkill#skill_name and UserSkill#skill_name= methods.

--- app/models/user_skill.rb
+++ app/models/user_skill.rb
@@ -3,4 +3,19 @@ class UserSkill < ActiveRecord::Base
   belongs_to :user
   belongs_to :skill
   
+  def skill_name
+    skill ? skill.name : nil
+  end
+  def skill_name=(new_value)
+    # OPTIMIZE: I believe indexing the name column of skills won't do any good here
+    # However, on sqlite3, search are very case sensitive so this matters
+    existing_skill = Skill.find(:first, :conditions => ["LOWER(name) = LOWER(?)", new_value])
+    
+    if !existing_skill
+      existing_skill = Skill.create(:name => new_value)
+    end
+    
+    self.skill_id = existing_skill.id
+  end
+  
 end
--- spec/models/user_skill_spec.rb
+++ spec/models/user_skill_spec.rb
@@ -23,4 +23,41 @@ describe UserSkill do
       @user_skill.should respond_to("skill")
     end
   end # associations
+  
+  describe "attributes" do
+    describe "skill name" do
+      before(:each) do
+        @skill = mock_model(Skill, :name => "Rails")
+      end
+      
+      it "should get the skill name" do
+        @user_skill.stub!(:skill).and_return(@skill)
+        @user_skill.skill_name.should == "Rails"
+      end
+      it "should set the skill by name if a skill by that name exists" do
+        # The 'with' piece is freakin out on me :/
+        # Skill.should_receive(:find).with(:first, {:conditions => ["LOWER(name) = LOWER(?)", "Rails"]}).and_return(@skill)
+        Skill.should_receive(:find).twice.and_return(@skill)
+        
+        @user_skill.skill_name = "Rails"
+        @user_skill.skill.should == @skill
+      end
+      it "should set the skill by name if a skill by that name exists (even if it's different case type)" do
+        # The 'with' piece is freakin out on me :/
+        # Skill.should_receive(:find).with(:first, {:conditions => ["LOWER(name) = LOWER(?)", "raIlS"]}).and_return(@skill)
+        Skill.should_receive(:find).twice.and_return(@skill)
+        
+        @user_skill.skill_name = "raIlS"
+        @user_skill.skill.should == @skill
+      end
+      it "should create a new skill based on the name passed in if that name does not exist" do
+        Skill.should_receive(:find).and_return(nil)
+        
+        Skill.should_receive(:create).and_return(@skill)
+        @user_skill.skill_name = "Rails"
+      end
+    end # skill name
+        
+
+  end #attributes
 end
