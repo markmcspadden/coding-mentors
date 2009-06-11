173fabeb7d7e394f80a6a3c72491ed06178a3857
Mark McSpadden <markmcspadden@gmail.com>
2009-06-11 14:13:32 -0500

    Filter out password params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password, :password_confirmation
+  filter_parameter_logging :password, :password_confirmation
   
   # before_filter :login_from_cookie
   # before_filter :login_required
