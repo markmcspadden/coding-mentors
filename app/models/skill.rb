e8afd831a7c93997b125dcbbad16122c4785285e
Mark McSpadden <markmcspadden@gmail.com>
2009-06-12 17:19:41 -0500

    Add seven levels to skills

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -6,5 +6,29 @@ class Skill < ActiveRecord::Base
   def to_s
     "#{name}"
   end
+  
+  class << self
+    
+    def levels
+      [ ["Innocent", 1],
+        ["Exposed", 2],
+        ["Apprentice", 3],
+        ["Practitioner", 4],
+        ["Journeyman", 5],
+        ["Master", 6],
+        ["Researcher", 7] ]      
+    end
+    
+    def level_descriptions
+      [ ["Innocent", "I know it exists"], 
+        ["Exposed", "I know I need it"],
+        ["Apprentice", "I've tried it with supervision"],
+        ["Practitioner", "I've been trained and used it a few times"],
+        ["Journeyman", "I've got this down"],
+        ["Master", "I know this inside and outside"],
+        ["Researcher", "I'm working on improving the skill itself"] ]
+    end
+    
+  end
 
 end
