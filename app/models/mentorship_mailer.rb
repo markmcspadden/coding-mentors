c61d3f09a6d61d6de194bbffdd3019b9f7042ca1
Mark McSpadden <markmcspadden@gmail.com>
2009-08-01 17:16:12 -0500

    Add emails for an accepted mentorship. (Not hooked into Mentorship lifecycle)

--- app/models/mentorship_mailer.rb
+++ app/models/mentorship_mailer.rb
@@ -8,21 +8,51 @@ class MentorshipMailer < ActionMailer::Base
                                 end
 
   def new_mentorship_to_mentor(mentorship)
+    setup_email
+    
     @receiver = mentorship.receiver
     @sender = mentorship.sender
     
     @skills = mentorship.skills.collect{ |s| s.name }.to_sentence
     @mentorship = mentorship
-    
-    @from = system_email
+
     @recipients = @receiver.email
-    @bcc = support_email
-    @content_type = "text/html"
-    
     @subject = "#{@sender.name} would like to be mentored by you!"
   end
   
+  def accepted_mentorship_to_mentee(mentorship)
+    setup_email
+    
+    @receiver = mentorship.mentee
+    @sender = mentorship.mentor
+    
+    @skills = mentorship.skills.collect{ |s| s.name }.to_sentence
+    @mentorship = mentorship    
+    
+    @recipients = @receiver.email
+    @subject = "#{@sender.name} has agreed to mentor you!"    
+  end
+  
+  def accepted_mentorship_to_mentor(mentorship)
+    setup_email
+    
+    @receiver = mentorship.mentor
+    @sender = mentorship.mentee
+    
+    @skills = mentorship.skills.collect{ |s| s.name }.to_sentence
+    @mentorship = mentorship    
+    
+    @recipients = @receiver.email
+    @subject = "#{@sender.name} has agreed to be your apprentice!"    
+  end  
+  
   protected
+  def setup_email
+    @from = system_email
+    @bcc = support_email
+    @content_type = "text/html"
+  end
+  
   def system_email
     "Coding Mentors <system@codingmentors.com>"
   end
--- spec/models/mentorship_mailer_spec.rb
+++ spec/models/mentorship_mailer_spec.rb
@@ -10,8 +10,8 @@ describe MentorshipMailer do
   before(:each) do
     setup_tmail
     
-    @sender = mock_model(User, :email => "sender@email.com", :name => "Mark McSpadden")
-    @receiver = mock_model(User, :email => "receiver@email.com", :name => "Dave Kruse")
+    @sender = mock_model(User, :email => "mark@email.com", :name => "Mark McSpadden")
+    @receiver = mock_model(User, :email => "dave@email.com", :name => "Dave Kruse")
     
     @s1 = mock_model(Skill, :name => "Ruby on Rails")
     @s2 = mock_model(Skill, :name => "Ruby")
@@ -21,7 +21,9 @@ describe MentorshipMailer do
                                           :mentee => @sender, 
                                           :sender => @sender, 
                                           :receiver => @receiver,
-                                          :skills => [@s1, @s2, @s3])
+                                          :skills => [@s1, @s2, @s3],
+                                          :sender_note => "Dave, I'm a big fan. Please help me!",
+                                          :receiver_note => "Sounds like fun. Let's do this!")
     @mentorship.stub!(:to_param).and_return("1")
   end
   
@@ -34,6 +36,31 @@ describe MentorshipMailer do
     
     MentorshipMailer.create_new_mentorship_to_mentor(@mentorship).encoded.should == @expected.encoded      
   end
+  
+  describe "an accepted mentorship" do
+    it "should send an accepted mentorship email to the mentee" do  
+      @expected.subject = 'Dave Kruse has agreed to mentor you!'
+      @expected.bcc     = 'Coding Mentors <support@codingmentors.com>'
+      @expected.from    = 'Coding Mentors <system@codingmentors.com>'
+      @expected.to      = @sender.email
+      @expected.body    = read_fixture('accepted_mentorship_to_mentee')
+
+      MentorshipMailer.create_accepted_mentorship_to_mentee(@mentorship).encoded.should == @expected.encoded      
+    end
+    it "should send an accepted mentorship email to the mentee" do  
+      @expected.subject = 'Mark McSpadden has agreed to be your apprentice!'
+      @expected.bcc     = 'Coding Mentors <support@codingmentors.com>'
+      @expected.from    = 'Coding Mentors <system@codingmentors.com>'
+      @expected.to      = @receiver.email
+      @expected.body    = read_fixture('accepted_mentorship_to_mentor')
+
+      MentorshipMailer.create_accepted_mentorship_to_mentor(@mentorship).encoded.should == @expected.encoded      
+    end    
+  end # an accepted mentorship
+  
+
+  
+  
   
   def setup_tmail
     # You don't need these lines while you are using create_ instead of deliver_
