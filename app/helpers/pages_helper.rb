9e358dc6d0a627502fa79a5f366f1da9a9bd39b7
Mark McSpadden <markmcspadden@gmail.com>
2009-08-02 21:03:22 -0500

    Fix bug with PagesHelper#title_skills in a blank state situation.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,8 +1,8 @@
 module PagesHelper
   
   def title_skills(learn_skill, teach_skill)
-    learn_text = content_tag("span", h(learn_skill.name), :class => "title_skill")
-    teach_text = content_tag("span", h(teach_skill.name), :class => "title_skill")
+    learn_text = content_tag("span", h(learn_skill.to_s), :class => "title_skill")
+    teach_text = content_tag("span", h(teach_skill.to_s), :class => "title_skill") 
     
     "Learn #{learn_text} &nbsp;&nbsp;<small>or</small>&nbsp;&nbsp; Teach #{teach_text}"
   end
--- spec/helpers/pages_helper_spec.rb
+++ spec/helpers/pages_helper_spec.rb
@@ -8,11 +8,18 @@ describe PagesHelper do
     included_modules.should include(PagesHelper)
   end
   
-  it "should create some pretty title skill text" do
-    @s1 = mock_model(Skill, :name => "Ruby")
-    @s2 = mock_model(Skill, :name => "Erlang")
+  describe "title skills" do
+    it "should create some pretty title skill text" do
+      @s1 = mock_model(Skill, :to_s => "Ruby")
+      @s2 = mock_model(Skill, :to_s => "Erlang")
+
+      helper.title_skills(@s2, @s1).should == "Learn <span class=\"title_skill\">Erlang</span> &nbsp;&nbsp;<small>or</small>&nbsp;&nbsp; Teach <span class=\"title_skill\">Ruby</span>"
+    end
     
-    helper.title_skills(@s2, @s1).should == "Learn <span class=\"title_skill\">Erlang</span> &nbsp;&nbsp;<small>or</small>&nbsp;&nbsp; Teach <span class=\"title_skill\">Ruby</span>"
-  end
+    it "should handle the case where the skills are both nil" do
+      helper.title_skills(nil, nil).should == "Learn <span class=\"title_skill\"></span> &nbsp;&nbsp;<small>or</small>&nbsp;&nbsp; Teach <span class=\"title_skill\"></span>"
+    end
+  end # title skills
+
 
 end
